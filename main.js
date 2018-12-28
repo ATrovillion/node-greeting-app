8db6e9bd01efc9db28e7cb8451872410ca4c932a
Aaron Trovillion <aaron.trovillion@gmail.com>
2018-12-28 12:52:09 -0500

    initial commit with greetings and farewells

---
+++ main.js
@@ -0,0 +1,20 @@
+const greetings = require("./greeting.js");
+const farewells = require("./farewells.js");
+
+//"hello"
+greetings.greetingEnglish();
+
+//"konichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Halló"
+greetings.greetingIcelandic();
+
+//"Goodbye"
+farewells.farewellEnglish();
+
+//"Adios"
+farewells.farewellSpanish();
